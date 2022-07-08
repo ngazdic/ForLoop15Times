780f19117df8a3babdca2707eaeb44cd427fb328
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-08 12:23:46 -0400

    for loop added 15 times manually

---
+++ ForLoop15times/Program.cs
@@ -0,0 +1,89 @@
+﻿#region LoopExcersise;
+static void ExcersiseLoop15Times()
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+    Console.WriteLine($"{i}");
+
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+                    
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+
+    }
+
+    
+
+}
+
+#endregion LoopExcersise;
+
